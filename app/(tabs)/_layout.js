86e6858156aeffefecf494b8efdc44a82a16f305
joelyy <j.paredes.psa@gmail.com>
2024-09-07 18:38:29 +0800

    Pokedex finished but can be changed or make it better

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -1,15 +1,22 @@
+import React from 'react';
+import { useTheme } from '@react-navigation/native';
 import { Tabs } from 'expo-router';
-import { Ionicons } from '@expo/vector-icons'; 
+import { Ionicons, FontAwesome } from '@expo/vector-icons';
 
 export default function TabLayout() {
+  const { colors } = useTheme();
+
   return (
     <Tabs>
       <Tabs.Screen
         name='index'
         options={{
           title: 'Pokémon List',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="list" color={color} size={size} />
+          tabBarIcon: ({ color, size, focused }) => (
+            <FontAwesome
+              name="circle"
+              color={focused ? 'red' : colors.icon} 
+            />
           ),
         }}
       />
@@ -17,8 +24,12 @@ export default function TabLayout() {
         name='groupList'
         options={{
           title: 'Group List',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="people" color={color} size={size} />
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name="people"
+              color={focused ? 'red' : colors.icon} 
+              size={size}
+            />
           ),
         }}
       />
@@ -26,8 +37,12 @@ export default function TabLayout() {
         name='favorites'
         options={{
           title: 'Favorites',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="heart" color={color} size={size} />
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name="heart"
+              color={focused ? 'red' : colors.icon} 
+              size={size}
+            />
           ),
         }}
       />
@@ -35,8 +50,12 @@ export default function TabLayout() {
         name='Settings'
         options={{
           title: 'Settings',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="settings" color={color} size={size} />
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name="settings"
+              color={focused ? 'red' : colors.icon} // Use theme color for non-focused state
+              size={size}
+            />
           ),
         }}
       />
